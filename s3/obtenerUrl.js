e1da3294f519de653130948419d9a72fe52e8d0a
alex20276 <axeltoa780@gmail.com>
2025-06-13 09:25:34 -0500

    ayudaaaaaaaaaaaaaaa

---
+++ s3/obtenerUrl.js
@@ -0,0 +1,18 @@
+const { GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const s3 = require('./s3client');
+require("dotenv").config();
+
+
+async function obtenerUrlArchivo (nombreArchivo) {
+  const comando = new GetObjectCommand({
+    Bucket: process.env.AWS_BUCKET,
+    Key: "LuisFer"+nombreArchivo,
+  });
+
+  const url = await getSignedUrl(s3, comando, { expiresIn: 60 }); // segundos
+
+  return url;
+};
+
+module.exports = obtenerUrlArchivo;
